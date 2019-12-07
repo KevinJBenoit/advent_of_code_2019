130cb0841770faab0360b49e0ac319e11cf7ebb0
Kevin Benoit <kevin.benoit8@gmail.com>
2019-12-07 11:58:48 -0700

    added 3 and 4 opcodes

--- day5/day5.py
+++ day5/day5.py
@@ -16,6 +16,7 @@ def compute_opcodes(codes, program):
     elif position_0 == 2:
         multiply_opcode(codes, program)
 
+    #if position 3 only use 2 codes
     elif position_0 == 3:
         take_input(codes, program)
 
@@ -47,50 +48,76 @@ def multiply_opcode(codes, program):
 
     program[position_3] = program[position_1] * program[position_2]
 
+def take_input(codes, program):
+    """
+    protocol if position 0 is a 3, will ask for an integer input value from user
+    and store it at the address provided by position 1
+    """
+    integer_input_value = input("Please provide an input: ")
+    position_1 = codes[1]
 
-def program_output(intcode_in, noun, verb):
+    program[position_1] = integer_input_value
+
+def output_parameter(codes, program):
+    """
+    protocol if position 0 is a 4, will immediately output the value that is stored
+    at the address provided by position 1
+    """
+    position_1 = codes[1]
+
+    output = program[position_1]
+
+    return output
+
+
+def parameter_mode(parameters, codes, program):
+    """
+    takes the ABCDE paraemters and performs proper computations
+    """
+    #break down the parameters
+    list_parameter = [int(digit) for digit in str(parameters)]
+
+    #check if there is a 3rd parameter
+    if len(list_parameter) == 4:
+        #DE
+        two_digit_opcode = list_parameter[2:]
+        #C
+        first_parameter = list_parameter[1]
+        #D
+        second_parameter = list_parameter[0]
+
+
+def program_output(intcode_in):
     """
     produces the output of the program with the given noun and verb
     """
     list_intcode = list(intcode_in)
     flag = True
     i = 0
-    list_intcode[1] = noun
-    list_intcode[2] = verb
+
     #slice through the program 4 codes at a time
+    #if position 0 is 3, slice 2 codes
     while flag:
-        flag = flag = compute_opcodes(list_intcode[0+i:4+i], list_intcode)
+        if list_intcode[0+i] == 3:
+            compute_opcodes(list_intcode[0+i:2+i], list_intcode)
+        #compute the next 4 codes
+        else:
+            flag = compute_opcodes(list_intcode[0+i:4+i], list_intcode)
         i += 4
 
     return list_intcode[0]
 
 
-def output_parameter(codes, program):
-    return codes[1]
-
-def take_input(codes, program):
-    print("test")
 
 def main():
     """
     Copied over code from Day 2 for modification
     """
-    flag = False
-    noun, verb = 0, 0
 
-    while not flag:
-        output = program_output(intcode, noun, verb)
+    output = program_output(intcode)
 
-        if output == 19690720:
-            flag = True
-        elif 19690720 - output < 99:
-            verb = 19690720 - output
-            flag = True
-        else:
-            noun += 1
+    print(output)
 
-    print(f"Output: {output}, noun: {noun}, verb:{verb}")
-    print(f"Answer: {100 * noun + verb}")
 
 if __name__ == "__main__":
     main()
